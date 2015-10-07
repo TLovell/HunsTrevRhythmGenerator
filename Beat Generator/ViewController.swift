7f11a87958f10342e9ea27f1067688c4768e86f2
Trevor Lovell <trevorlovell@Trevors-Macbook-Pro.local>
2015-10-07 08:08:01 -0600

    Initial Commit

---
+++ Beat Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beat Generator
+//
+//  Created by Trevor Lovell on 10/7/15.
+//  Copyright (c) 2015 Trevor Lovell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
